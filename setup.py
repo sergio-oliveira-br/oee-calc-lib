94655f7e05c4cd149a2e870a4de0c8a06a91b2ef
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-19 12:48:32 +0100

    project created  - just the structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-oee-calc',
+    version='1.0.0',
+    packages=find_packages(),
+    description='A simple Python OEE Calculator',
+    author='Sergio Oliveira',
+    python_requires='>=3.8',
+)
